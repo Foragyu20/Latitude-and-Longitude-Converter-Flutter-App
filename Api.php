c0e257f70adc77e7dff263567ad8e7cb3a63e632
Foragyu20 <cforestraguini@gmail.com>
2024-12-13 14:34:57 +0800

    Initialize PHP script with database connection setup

---
+++ Api.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "coordinates_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die(json_encode(["status" => "error", "message" => "Database connection failed"]));
+}
