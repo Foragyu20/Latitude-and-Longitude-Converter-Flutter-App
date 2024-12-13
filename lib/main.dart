c339c8bf6a626d05e4f427e9146a0918902834c3
Foragyu20 <cforestraguini@gmail.com>
2024-12-13 13:41:22 +0800

    making the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const LatLngConverterApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class LatLngConverterApp extends StatefulWidget {
+  const LatLngConverterApp({super.key});
 
   @override
+  LatLngConverterAppState createState() => LatLngConverterAppState();
+}
+
+class LatLngConverterAppState extends State<LatLngConverterApp> {
+final TextEditingController _latitudeController = TextEditingController();
+  final TextEditingController _longitudeController = TextEditingController();
+  
+  
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(child: ,),
-        ),
-      ),
-    );
+    return Scaffold();
   }
+
 }
